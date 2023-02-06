b50bfaf8a333a645993f95a200326c63dae27896
Liutsiia <lusi_600@mail.ru>
2023-02-06 10:05:25 -0500

    lesson 2

---
+++ js/script.js
@@ -0,0 +1,21 @@
+console.log('Hello!'); // stroki berem v kovichki
+console.log("Hello world!");
+console.log('333');
+console.log(334);
+console.log('Hello'+' '+'world!');
+// alert('hello')
+
+document.getElementById('out').innerHTML = "Hello";
+document.getElementById('out').innerHTML = 2022;
+document.getElementById('out').innerHTML = '<b>2023</b>';
+
+document.querySelector('.header').innerHTML =  15;
+document.querySelector('#one').innerHTML =  777;
+
+let b;
+let a = document.querySelector('#one');
+let c;
+c = document.querySelector('.header');
+
+a.innerHTML = 9999;
+c.innerHTML = 6666;
