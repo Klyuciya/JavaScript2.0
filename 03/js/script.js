7674eed4015fa0e628e843e80d7c2b6d45f22a5b
Liutsiia <lusi_600@mail.ru>
2023-03-08 09:56:42 -0500

    lesson 3

---
+++ 03/js/script.js
@@ -0,0 +1,43 @@
+let a = 6;
+
+// >= <= == !=
+// if (a > 9){
+//    // true
+//    console.log("Yes");
+// }
+// else {
+//     console.log("else")
+// }
+
+const button = document.querySelector('button');
+const input = document.querySelector('.age');
+
+
+// () => стрелочная функция
+button.onclick = () => {
+    let num = +input.value;
+    if (num >= 16 && num < 60) {
+        console.log ("Welcome!!!");
+    }
+    else if (num > 60) { 
+        console.log(" Ты точно сюда? ");
+    }
+    else {
+        console.log ("Ты не пройдешь!!!")
+    }
+
+    switch (num) {
+        case 15:
+            console.log("Еще год потерпи!");
+            break;
+        case 16:
+            console.log("Уже можно!!!");
+            break;
+        default:
+            console.log('oook');
+    }
+}
+
+let b = 5;
+
+console.log(b > 3 && b < 7); // && - и  || - или
