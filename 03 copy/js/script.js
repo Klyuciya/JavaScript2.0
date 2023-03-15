903d5e8e078f2f29ab90e505945c07f68d7c79d7
Liutsiia <lusi_600@mail.ru>
2023-03-15 10:06:20 -0400

    home-work-lesson3-task9-14

--- 03 copy/js/script.js
+++ 03 copy/js/script.js
@@ -130,18 +130,111 @@ document.querySelector('.out-7').innerHTML = Math.pow(n71,n72)
 const b8 = document.querySelector('.b-8');
 const s8 = document.querySelector('.s-8');
 b8.onclick = () => {
-    let x = 0;
     let n8=+s8.value;
     switch(n8) {
         case 1: 
-            document.querySelector('.out-8').innerHTML = 1;
+            document.querySelector('.out-8').innerHTML = "one";
             break;
         case 2: 
-            document.querySelector('.out-8').innerHTML = 2;
+            document.querySelector('.out-8').innerHTML = "two";
             break;
         case 3: 
-            document.querySelector('.out-8').innerHTML = 3
+            document.querySelector('.out-8').innerHTML = "three"
             break;
     }
 }
 
+
+// Task 9.
+
+// Создайте на странице input[type=number] с классом i-9, куда пользователь может ввести номер квартиры. Есть кнопка b-9 которая запускает функцию f9. Функция должна вывести в .out-9 номер подъезда, в котором находится квартира.
+
+// если от 1 до 32 - то вывести цифру 1
+// если от 33 до 43 - то вывести 2
+// если от 44 до 64 - то 3.
+// В противном случае, вывести 0.
+
+const b9 = document.querySelector('.b-9');
+const i9 = document.querySelector('.i-9');
+b9.onclick = () => {
+    let n9=+i9.value;
+    if ( n9>=1 && n9<=32) {
+        document.querySelector('.out-9').innerHTML = 1
+    } else if (n9<=43){
+        document.querySelector('.out-9').innerHTML = 2
+    } else if (n9<=64) {
+        document.querySelector('.out-9').innerHTML = 3
+    } else {
+        document.querySelector('.out-9').innerHTML = 0
+    }
+}
+       
+// Task 10.
+
+// Дан select s-100. По нажатию кнопки, выведите value выбранного option в out-10.
+
+const b10 = document.querySelector('.b-10');
+const s100 = document.querySelector('.s-100');
+b10.onclick = () => {
+    let n10 = s100.value;
+    document.querySelector('.out-10').innerHTML = n10;
+}
+
+// Task 11.
+
+// Дан select s-110. По изменению состояния select (событие onchange) выведите value выбранного option в out-11.
+
+const s110 = document.querySelector('.s-110');
+s110.onchange = () => {
+    let n11 = s110.value;
+    document.querySelector('.out-11').innerHTML = n11;
+}
+
+
+// Task 12.
+
+// Дан input i-120. По нажатию кнопки получите значение из input в переменную, а затем выведите в out-12 typeof полученной переменной. Typeof позволяет определить тип данных. Обратите внимание, данная задача уже решена, нужно убрать комментарии и изучить работу.
+
+const i120 = document.querySelector('.i-120');
+const b12 = document.querySelector('.b-12');
+b12.onclick = () => {
+    n12 = i120.value;
+    document.querySelector('.out-12').innerHTML = typeof n12
+}
+
+// Task 13.
+
+// Дан input i-130. В отличие от предыдущего задания - input type number. По нажатию кнопки получите значение из input в переменную, а затем выведите в out-13 typeof полученной переменной. Typeof позволяет определить тип данных. Если вы правильно все сделали - то удивительно, но тип данных будет string! Подумайте почему так?
+
+const i130 = document.querySelector('.i-130');
+const b13 = document.querySelector('.b-13');
+b13.onclick = () => {
+    n13 = i130.value;
+    document.querySelector('.out-13').innerHTML = typeof n13
+}
+
+// Task 14.
+
+// Дан input i-141 и input-142, type=number. Дан select s-143, который содержит две операции - +, -, *, / . Дана кнопка b-14, при нажатии на которую срабатывает функция f14. Функция выводит в out-14 результат операций выбранной в 3-м select к числам введенным в первом и втором input. Например выбрано 1 13 +, нужно вывести результат операции 1+13 т.е. 14.
+
+function f14 () {
+    const i141 = +document.querySelector('.i-141').value;
+    const i142= +document.querySelector('.i-142').value;
+    const s143= document.querySelector('.s-143').value;
+    let out14 = document.querySelector('.out-14');
+    switch(s143) {
+        case "+" :
+            out14.innerHTML = i141+i142;
+            break;
+        case "-" :
+            out14.innerHTML = i141-i142;
+            break;
+        case "*" :
+            out14.innerHTML = i141*i142;
+            break;
+        case "/" :
+            out14.innerHTML = i141/i142;
+            break;
+    }
+}
+document.querySelector('.b-14').onclick = f14;
