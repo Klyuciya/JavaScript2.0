0bfaad1ec5ca212816a45150702fda950e7cf62d
Liutsiia <lusi_600@mail.ru>
2023-03-16 10:52:02 -0400

    home work lesson 3 task 15

--- 03 copy/js/script.js
+++ 03 copy/js/script.js
@@ -238,3 +238,23 @@ function f14 () {
     }
 }
 document.querySelector('.b-14').onclick = f14;
+
+// Task 15.
+
+// Дан select s-151 и s-152, каждый из которых содержит 1 и 0. Дан select s-153, который содержит две операции - && и || . Дана кнопка b-15, при нажатии на которую срабатывает функция f15. Функция выводит в out-15 результат логических операций выбранных в 3 select к числам выбранным в первом и втором select. Например выбрано 1 1 &&, нужно вывести результат операции 1&&1 т.е. 1 или 0.
+
+function f15 () {
+    const s151 = +document.querySelector('.s-151').value;
+    const s152= +document.querySelector('.s-152').value;
+    const s153= document.querySelector('.s-153').value;
+    let out15 = document.querySelector('.out-15');
+    switch(s153) {
+        case "||" :
+            out15.innerHTML = s151||s152;
+            break;
+        case "&&" :
+            out15.innerHTML = s151&&s152;
+            break
+    }
+}
+document.querySelector('.b-15').onclick = f15;
