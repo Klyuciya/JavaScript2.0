a5429341e369960a4a0fe0f21c7cd858bd98a830
Liutsiia <lusi_600@mail.ru>
2023-04-05 12:04:05 -0400

    added lesson 08 task  01-15

---
+++ 08/js/script.js
@@ -0,0 +1,45 @@
+for (let i = 0; i < 6; i++ ) {
+    console.log(i);
+}
+
+let k = 0;
+while ( k < 5) {
+    k++;
+    console.log ('k: ' +k);
+}
+
+// 0 + ... + 10
+
+let sum = 0;
+let p = 0;
+while (p <= 10) {
+    sum = sum + p;
+    p++;
+}
+console.log ( 'sum = ' + sum)
+
+// ****
+// ****
+// ****
+// ****
+
+p = 0;
+let out = document.querySelector( '.out');
+let outStr = '';
+let flag = 3;
+
+while ( p < 4) {
+    let p1 = 0;
+    while ( p1 < 4) {
+        if ( p1 < flag) {
+            outStr += '&nbsp';
+        } else {
+            outStr += '*';
+        }
+        p1++;
+    }
+    flag--;
+    outStr += '<br>';
+    p++
+}
+out.innerHTML = outStr;
